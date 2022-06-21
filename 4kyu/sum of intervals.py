3654a4f06114fe1e9e458ee9abbfbab2e947a6bc
mfaiquzair <83383135+mfaiquzair@users.noreply.github.com>
2022-06-21 22:56:45 +0800

    Update sum of intervals.py

--- 4kyu/sum of intervals.py
+++ 4kyu/sum of intervals.py
@@ -1,10 +1,10 @@
-def sum_of_intervals(intervals):
+def sum_of_intervals(intervals): #define function
     numbers = set()
 
-    for start, stop in intervals:
+    for start, stop in intervals: #for loop utk start stop
         current = start
         while current < stop:
             numbers.add(current)
             current += 1
 
-    return len(numbers)
+    return len(numbers) #len return number
