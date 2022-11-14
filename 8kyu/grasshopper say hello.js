243e14de9fe9813753c87658c99f5a3a7a1e4386
mfaiquzair <83383135+mfaiquzair@users.noreply.github.com>
2022-11-14 22:16:09 +0800

    Create grasshopper say hello.js

---
+++ 8kyu/grasshopper say hello.js
@@ -0,0 +1,17 @@
+function sayHello(name) {
+    return `Hello, ${name}`
+  }
+  /*
+  function sayHello(faiq) {
+  return `Hello, ${faiq}`
+}
+*/
+  /*
+  const person = {
+    name: "Mr. Spock"
+  };
+  
+  const sayHello = `Hello, ${person.name}`;
+  
+  console.log(sayHello);
+  */
